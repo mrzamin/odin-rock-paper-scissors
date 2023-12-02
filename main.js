05b0aebd62c360cf1bc039fedebd330a6b5a37c5
mrzamin <142754418+mrzamin@users.noreply.github.com>
2023-12-02 18:46:03 -0500

    Modify index.html and main.js

--- main.js
+++ main.js
@@ -2,11 +2,21 @@
 const choices = ["rock", "paper", "scissors"];
 const winners = []; //Array with any combination of "Player", "Winner", and "Tie"
 
-// Write a function called getComputerChoice that randomly selects rock, paper, or scissors for the computer and prints to console.*/
+// Randomly selects rock, paper, or scissors for the computer.
 function getComputerChoice () {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
+function showReset () {
+    const resetBtn = document.createElement('button');
+    resetBtn.textContent = "Play again?";
+    resetBtn.addEventListener ('click', resetGame);
+    const gameWinner = document.querySelector('.gamewinner');
+    gameWinner.appendChild(resetBtn);
+}
+
+
+//Resets the game and scoreboard items to 0.
 function resetGame () {
     winners = [];
     document.querySelector('.playerchoice').textcontent = "Player Choice: ";
@@ -16,12 +26,13 @@ function resetGame () {
     document.querySelector('.ties').textcontent = "Ties: 0";
     document.querySelector('.roundwinner').textcontent = "Round Winner: ";
     document.querySelector('.winner').textcontent = "Winner: ";
+};
 
-}
-
-//Start the game when a selection is clicked on the UI.
+//Starts the game when a selection is clicked on the UI.
 function startGame () {
     let buttons = document.querySelectorAll('button');
+    const resetBtn = document.querySelector('.reset');
+    resetBtn.style.display = 'none';
     buttons.forEach((button) =>
     button.addEventListener (('click'), () => {
         if(button.id) {
@@ -30,12 +41,16 @@ function startGame () {
     }))
 
 }
-// Result of startGame() gets passed in. Write a function that plays the round and ???
+// User selection from startGame() gets passed in. Plays the round.
 function playRound (playerSelection) {
+    
     let wins = checkWins();
+    
     if (wins >= 5) {
-        return
+        return 
     }
+
+    
     const computerSelection = getComputerChoice();
     const winner = checkWinner (playerSelection, computerSelection);
     winners.push(winner);
@@ -43,32 +58,32 @@ function playRound (playerSelection) {
     displayChoice(playerSelection, computerSelection, winner);
     wins = checkWins();
     if (wins == 5) {
-        displayEnd();
+            displayEnd();
     }
+
 }
 
-//Write a function that displays the winner at the end on the UI.
-
+//Displays the winner at the end on the UI.
 function displayEnd () {
     let playerIsWinner = winners.filter((item) => item == "Player").length;
     if (playerIsWinner == 5){
         document.querySelector('.gamewinner').textContent = "You won five games, congrats!";
+        showReset();
     }
     else {
         document.querySelector('.gamewinner').textContent = "The computer won five games.";
+        showReset();
     }
-
 }
 
-//Write a function that displays each player's choice on the UI. 
-
+//Displays each player's choice on the UI. 
 function displayChoice (playerSelection, computerSelection, winner) {
     document.querySelector('.playerchoice').textContent = `You chose: ${playerSelection}`
     document.querySelector('.computerchoice').textContent = `Computer chose: ${computerSelection}`
     document.querySelector('.roundwinner').textContent = `Round Winner: ${winner}`;
 }
 
-//Write a function that updates the score on the UI of player wins, computer wins, and ties.
+//Updates the score on the UI of player wins, computer wins, and ties.
 function updateScore (playerSelection, computerSelection, winner) {
     let pWinCount = winners.filter((item) => item == "Player").length;
     let cWinCount = winners.filter((item) => item == "Computer").length;
@@ -79,7 +94,7 @@ function updateScore (playerSelection, computerSelection, winner) {
 }
 
 
-//Write a function that determines the winner of a round.
+//Checks the winner of a round.
 function checkWinner (playerSelection, computerSelection) {
     if (playerSelection == computerSelection) 
     {
@@ -98,6 +113,7 @@ function checkWinner (playerSelection, computerSelection) {
     }
 }
 
+//Helper function that returns the highest number of wins.
 function checkWins () {
     let pWinCount = winners.filter((item) => item == "Player").length;
     let cWinCount = winners.filter((item) => item == "Computer").length;
